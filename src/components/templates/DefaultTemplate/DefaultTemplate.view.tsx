f0ddbf1c5912bf3bcb4ee662ce727f1637cb1f3d
Gabriel Mochi <gmochi56@icloud.com>
2023-07-01 17:56:14 -0400

    default template change from div to fragment

--- src/components/templates/DefaultTemplate/DefaultTemplate.view.tsx
+++ src/components/templates/DefaultTemplate/DefaultTemplate.view.tsx
@@ -1,12 +1,11 @@
 import type { ReactElement } from "react";
-import { Box } from "@mui/material";
 import type { DefaultTemplateProps } from "./index";
 import Header from "../../organisms/Header";
 
 const DefaultTemplateView = ({ header }: DefaultTemplateProps): ReactElement => (
-  <Box>
+  <>
     <Header {...header} />
-  </Box>
+  </>
 );
 
 export default DefaultTemplateView;
