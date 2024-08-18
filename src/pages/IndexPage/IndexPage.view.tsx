5a7356eafd4676754b4fc1f3b986ce5b86ae960b
Gabriel Mochi <gmochi56@icloud.com>
2024-08-18 15:43:20 -0400

    adjust grid to xs devices

--- src/pages/IndexPage/IndexPage.view.tsx
+++ src/pages/IndexPage/IndexPage.view.tsx
@@ -5,16 +5,24 @@ import Header from "../../components/organisms/Header";
 import Sidebar from "../../components/organisms/Sidebar";
 import Main from "../../components/organisms/Main";
 
+const SidebarGrid = (): ReactElement => (
+  <Grid xs={12} maxWidth={{ md: "384px" }}>
+    <Sidebar />
+  </Grid>
+);
+
+const MainGrid = (): ReactElement => (
+  <Grid xs={12}>
+    <Main />
+  </Grid>
+);
+
 const IndexPageView = (): ReactElement => (
   <Box display="flex" flexDirection="column" flex="1">
     <Header />
     <Grid container flex="1">
-      <Grid xs={3} maxWidth="384px">
-        <Sidebar />
-      </Grid>
-      <Grid xs>
-        <Main />
-      </Grid>
+      <SidebarGrid />
+      <MainGrid />
     </Grid>
   </Box>
 );
