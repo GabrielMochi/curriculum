e1af091e8c0069d1eeeb0351d25438b3f6b6cd5a
Gabriel Mochi <gmochi56@icloud.com>
2024-08-18 22:14:56 -0400

    adapts sidebar to tablets

--- src/components/organisms/Sidebar/Sidebar.view.tsx
+++ src/components/organisms/Sidebar/Sidebar.view.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement } from "react";
-import { Stack } from "@mui/material";
+import { Box, Grid, Stack } from "@mui/material";
 import type { SidebarControllerReturn } from "./Sidebar.interface";
 import globalCss from "../../../theme/globalCss";
 import SidebarSection from "../../molecules/SidebarSection";
@@ -15,46 +15,61 @@ const SidebarView = ({
   academicEducationRecords,
   certificationRecords,
 }: SidebarControllerReturn): ReactElement => (
-  <Stack
+  <Box
     component="aside"
-    spacing={globalCss.verticalSpacing}
     bgcolor="secondary.main"
     height="100%"
     px={globalCss.horizontalSpacing}
     py={globalCss.verticalSpacing}
   >
-    <SidebarSection title={t("contact.label")}>
-      {contactRecords.map(({ label, value }) => (
-        <Contact key={label} label={label} value={value} />
-      ))}
-    </SidebarSection>
-    <SidebarSection title={t("social.label")}>
-      {socialRecords.map(({ label, url, logo }) => (
-        <Social key={url} label={label} url={url} logo={logo} />
-      ))}
-    </SidebarSection>
-    <SidebarSection title={t("academicEducation.label")}>
-      {academicEducationRecords.map(({ courseTitle, institutionName, period, isIncomplete }) => (
-        <AcademicEducation
-          key={courseTitle}
-          courseTitle={courseTitle}
-          institutionName={institutionName}
-          period={period}
-          isIncomplete={isIncomplete}
-        />
-      ))}
-    </SidebarSection>
-    <SidebarSection title={t("certifications.label")}>
-      {certificationRecords.map(({ certificationTitle, institutionName, year }) => (
-        <Certification
-          key={certificationTitle}
-          certificationTitle={certificationTitle}
-          institutionName={institutionName}
-          year={year}
-        />
-      ))}
-    </SidebarSection>
-  </Stack>
+    <Grid
+      container
+      rowSpacing={globalCss.verticalSpacing}
+      columnSpacing={globalCss.horizontalSpacing}
+    >
+      <Grid item xs={12} sm={6} md={12}>
+        <SidebarSection title={t("contact.label")}>
+          {contactRecords.map(({ label, value }) => (
+            <Contact key={label} label={label} value={value} />
+          ))}
+        </SidebarSection>
+      </Grid>
+      <Grid item xs={12} sm={6} md={12}>
+        <SidebarSection title={t("social.label")}>
+          {socialRecords.map(({ label, url, logo }) => (
+            <Social key={url} label={label} url={url} logo={logo} />
+          ))}
+        </SidebarSection>
+      </Grid>
+      <Grid item xs={12} sm={6} md={12}>
+        <SidebarSection title={t("academicEducation.label")}>
+          {academicEducationRecords.map(
+            ({ courseTitle, institutionName, period, isIncomplete }) => (
+              <AcademicEducation
+                key={courseTitle}
+                courseTitle={courseTitle}
+                institutionName={institutionName}
+                period={period}
+                isIncomplete={isIncomplete}
+              />
+            ),
+          )}
+        </SidebarSection>
+      </Grid>
+      <Grid item xs={12} sm={6} md={12}>
+        <SidebarSection title={t("certifications.label")}>
+          {certificationRecords.map(({ certificationTitle, institutionName, year }) => (
+            <Certification
+              key={certificationTitle}
+              certificationTitle={certificationTitle}
+              institutionName={institutionName}
+              year={year}
+            />
+          ))}
+        </SidebarSection>
+      </Grid>
+    </Grid>
+  </Box>
 );
 
 export default SidebarView;
