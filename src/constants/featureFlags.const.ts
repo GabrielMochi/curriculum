ef646283dcfe14e7d52e4afdd7762e1d41fdf87e
Gabriel Mochi <gmochi56@icloud.com>
2024-09-27 21:01:56 -0400

    hide certificates section

--- src/constants/featureFlags.const.ts
+++ src/constants/featureFlags.const.ts
@@ -2,7 +2,7 @@ export const FEATURE_FLAGS = {
   showContact: true,
   showSocial: true,
   showAcademicEducation: true,
-  showCertifications: true,
+  showCertifications: false,
   showAbout: true,
   showProfessionalExperience: true,
   showSkillsChips: false,
